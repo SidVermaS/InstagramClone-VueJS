218ae16295fd1219feb1f6368eab34188869261d
sidverma <siddharthvermas123456@gmail.com>
2020-07-04 16:37:59 +0530

    add

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -2,22 +2,15 @@
     <div class="custom_messages_background">
         <div v-for="(message, index) in messages" :key="index" class="mt-3"> 
             <span v-if="message.user.user_id===user_id" class="text-right d-block">
-                <!-- <b-col cols="7"></b-col>
-                <b-col cols="5"> -->
                 <span class="custom_self_message">{{message.message_text}}</span>
-                <!-- </b-col> -->
             </span>
             <span v-else class="text-left d-block">
-                <!-- <b-col cols="7"> -->
-                        <span v-if="index===0 || messages[(index-1)].user.user_id===message.user.user_id" class="">{{message.user.name}}</span>
-                        <!-- <span> -->
-                            <b-avatar v-if="index===(messages.length-1) || messages[(index+1)].user.user_id!==message.user.user_id" :src="`${baseUrlUserPhoto}${message.user.photo_url}`" size="1.4rem"></b-avatar>
-                            <b-avatar v-else size="1.4rem"   />
-                            <span style="width: 1.4rem; height: 1.4rem;"></span>  
-                            <span class="custom_other_message">{{message.message_text}}</span>
-                        <!-- </span> -->
-                <!-- </b-col>
-                <b-col cols="5"></b-col> -->
+                        <span v-if="index===0 || messages[(index-1)].user.user_id!==message.user.user_id" style="font-size: 12px;" class="d-block mb-2 ml-4 custom_faded_color">{{message.user.name}}</span>
+
+                        <b-avatar v-if="index===(messages.length-1) || messages[(index+1)].user.user_id!==message.user.user_id" :src="`${baseUrlUserPhoto}${message.user.photo_url}`" size="1.4rem"></b-avatar>
+                        <span v-else class="custom_other_message_margin"  />
+                        <span style="width: 1.4rem; height: 1.4rem;"></span>  
+                        <span class="custom_other_message">{{message.message_text}}</span>
             </span>
         </div>   
     </div>
@@ -58,6 +51,9 @@
         /* width: 100%;
         height: 100%; */
         /* overflow-y: scroll; */
+    }
+    .custom_other_message_margin    {
+        margin-left: 1.4rem;
     }
     ul  {
         list-style-type: none;
